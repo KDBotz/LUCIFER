49114eddeb24c74db2efa9caab64f8c5f8942039
KD Botz <116437604+KDBotz@users.noreply.github.com>
2022-11-15 20:17:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ class Bot(Client):
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             workers=50,
-            plugins={"root": "plugins"},
+            plugins={"root": "LUCIFER"},
             sleep_threshold=5,
         )
 
