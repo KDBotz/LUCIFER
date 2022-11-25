16e638999f299024b589a063faecbffdee9cb985
KD Botz <116437604+KDBotz@users.noreply.github.com>
2022-11-25 15:42:05 +0530

    Update pm_filter.py

--- LUCIFER/pm_filter.py
+++ LUCIFER/pm_filter.py
@@ -96,7 +96,7 @@ async def next_page(bot, query):
                 ),
                 InlineKeyboardButton(
                     text=f"{get_size(file.file_size)}",
-                    callback_data=f'files_#{req}#{file.file_id}',
+                    callback_data=f'files#{reqnxt}#{file.file_id}',
                 ),
             ]
             for file in files
