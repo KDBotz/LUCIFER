e6a8154c21d426d042a18f92bc2e918fe5652fd8
KD Botz <116437604+KDBotz@users.noreply.github.com>
2022-12-14 20:33:02 +0530

    Update pm_filter.py

--- LUCIFER/pm_filter.py
+++ LUCIFER/pm_filter.py
@@ -195,7 +195,8 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit(script.MVE_NT_FND)
+            k = await query.message.edit(script.MVE_NT_FND,
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Rᴇǫᴜᴇsᴛ ʜᴇʀᴇ", url=f"t.me/{SUPPORT_CHAT}")]])
             await asyncio.sleep(10)
             await k.delete()
 
